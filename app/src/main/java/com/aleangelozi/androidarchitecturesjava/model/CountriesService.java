32f3bcfc66e0cae58e1b4451be2bf60f936af905
aleangelozi <aleangelozi@gmail.com>
2021-06-04 14:25:56 -0300

    GsonConverterFactory not found fix

--- app/src/main/java/com/aleangelozi/androidarchitecturesjava/model/CountriesService.java
+++ app/src/main/java/com/aleangelozi/androidarchitecturesjava/model/CountriesService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import io.reactivex.Single;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CountriesService {
 
